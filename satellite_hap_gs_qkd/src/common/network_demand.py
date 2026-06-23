412983b876495eb870e3fea299b92d2c5ac12581
Zheng Zhang <11087832@polimi.it>
2026-06-23 18:44:59 +0200

    remove old package file

--- satellite_hap_gs_qkd/src/common/network_demand.py
+++
@@ -1,8 +0,0 @@
-"""Compatibility export for demand model.
-
-New code should import from ``services.service_demand``.
-"""
-
-from services.service_demand import Demand, ServiceDemand
-
-__all__ = ["Demand", "ServiceDemand"]
