cdd02ccb025aedfc3033bad5154857864ea1941d
Zheng Zhang <11087832@polimi.it>
2026-06-23 18:45:09 +0200

    remove old package file

--- satellite_hap_gs_qkd/src/common/network_scenario.py
+++
@@ -1,8 +0,0 @@
-"""Compatibility export for scenario model.
-
-New code should import from ``scenario.network_scenario``.
-"""
-
-from scenario.network_scenario import Scenario
-
-__all__ = ["Scenario"]
